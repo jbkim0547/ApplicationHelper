231e417384df93bb9829a3228204e5f3287dee10
jbkim0547 <75588499+jbkim0547@users.noreply.github.com>
2025-04-07 21:25:06 -0700

    Update AppDbContext.cs

--- Database/AppDbContext.cs
+++ Database/AppDbContext.cs
@@ -6,7 +6,7 @@ namespace ApplicationHelper.Database
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(option) { }
         
         public DbSet<InterviewNote> Interviews { get; set; }
         
