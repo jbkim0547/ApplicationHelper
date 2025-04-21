fca78f65c602dc4b4d09ef67ad0a6d4e72325835
jbkim0547 <75588499+jbkim0547@users.noreply.github.com>
2025-04-20 21:22:16 -0700

    Update ApplicationCalendarController.cs
    Fix typo.

--- Controllers/ApplicationCalendarController.cs
+++ Controllers/ApplicationCalendarController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace ApplicationHelper.Controlle
+namespace ApplicationHelper.Controllers
 {
     [Route("api/interviewNote")]
     [ApiController]
