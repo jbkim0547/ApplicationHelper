ce1e88628af5fc0c2ec4d1930ba94d55e6847bdd
jbkim0547 <75588499+jbkim0547@users.noreply.github.com>
2025-04-20 21:22:03 -0700

    Update ApplicationCalendarController.cs

--- Controllers/ApplicationCalendarController.cs
+++ Controllers/ApplicationCalendarController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace ApplicationHelper.Controllers
+namespace ApplicationHelper.Controlle
 {
     [Route("api/interviewNote")]
     [ApiController]
