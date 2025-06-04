50330ecfdfe94c5236aeb90582ee7ead453d71c6
jbkim0547 <75588499+jbkim0547@users.noreply.github.com>
2025-06-03 22:25:36 -0700

    Update AuthController.cs

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Text;;;
+using System.Text;
 
 namespace ApplicationHelper.Controllers
 {
